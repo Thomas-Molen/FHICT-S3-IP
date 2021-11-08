ce6c0518dd2721fbaf7dec84b04f77e78830caea
Thomas van der Molen <sugarypineapple2@gmail.com>
2021-11-08 13:23:23 +0100

    minor fix for gumroad

---
+++ backend-textadventure/textadventure_backend/textadventure_backend/Models/ProductVariants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace textadventure_backend.Models
+{
+    public class ProductVariants
+    {
+        public string Tier { get; set; }
+
+        public ProductVariants(string tierName)
+        {
+            Tier = tierName;
+        }
+    }
+}
